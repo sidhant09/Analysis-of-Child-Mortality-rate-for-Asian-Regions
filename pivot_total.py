d4724b52bd5d5b941ff397ae86cb2b4b8db10374
sidhant09 <sidhant.young@gmail.com>
2018-01-12 14:33:35 +0530

    Delete pivot_total.py

--- pivot_total.py
+++
@@ -1,8 +0,0 @@
-import pandas as  pd
-import csv
-from itertools import izip
-a = izip(*csv.reader(open("/home/sidhant/pro_new/pen_2/final_2/40/final_frames.csv", "rb")))
-csv.writer(open("pivot_total.csv", "wb")).writerows(a)
-df = pd.read_csv('pivot_total.csv')
-df1 = pd.DataFrame(df)
-df1.to_excel('pivot_total.xlsx')
