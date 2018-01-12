fd490af2ef61de75c0e3e2eb624ccd1f6edc984e
sidhant09 <sidhant.young@gmail.com>
2018-01-12 14:33:26 +0530

    Delete pivot_ic.py

--- pivot_ic.py
+++
@@ -1,8 +0,0 @@
-import pandas as  pd
-import csv
-from itertools import izip
-a = izip(*csv.reader(open("/home/sidhant/pro_new/pen_2/final_2/final_frames_1.csv", "rb")))
-csv.writer(open("pivot_ic.csv", "wb")).writerows(a)
-df = pd.read_csv('pivot_ic.csv')
-df1 = pd.DataFrame(df)
-df1.to_excel('pivot_ic.xlsx')
